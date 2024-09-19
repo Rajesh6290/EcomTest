68f54961ecf193faef798bd684639a742d58a7f8
Rajesh Kumar Behera <98143678+Rajesh6290@users.noreply.github.com>
2024-09-19 13:49:26 +0530

    first commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,25 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+      <link rel="icon" href="/favicon.ico" />
+        <meta name="subject" content="Shreyan E-commerce" />
+        <meta name="copyright" content="Shree Odisha Handloom" />
+        <meta name="category" content="Clothing (Brand)" />
+        <meta name="coverage" content="Worldwide" />
+        <meta name="distribution" content="Global" />
+        <meta name="rating" content="General" />
+        <meta name="author" content="Shree Odisha Handloom, shreeodishahandloom@gmail.com" />
+        <meta name="designer" content="SearchingYard Software Group" />
+        <meta name="owner" content="Shree Odisha Handloom" />
+        <meta name="language" content="ES" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,41 @@
+import { LogoSlider } from "@/components/common";
+import ClientTestimonial from "@/components/common/ClientTestimonial";
+import MobileApp from "@/components/common/MobileApp";
+import {
+  Coupons,
+  HeroSection,
+  NewArrival,
+  ProductImage,
+  SellingProduct,
+} from "@/components/home";
+import { useSwr } from "@/hooks";
+import { PublicLayout } from "@/layouts";
+
+export default function Home() {
+  const {
+    data: imageData,
+    isValidating,
+    mutate,
+  } = useSwr(`product/get-all-images`);
+  return (
+    <PublicLayout
+      title="Shree Odisha Handloom"
+      ogImage="https://shreyanecom-recent.vercel.app/logo.png"
+    >
+      <HeroSection />
+      <ProductImage />
+      <LogoSlider
+        title="Explore Our All Collections"
+        Arr={imageData}
+        mutate={mutate}
+        isValidating={isValidating}
+      />
+      <NewArrival />
+      <Coupons />
+      <SellingProduct />
+      {/* <Product /> */}
+      <MobileApp />
+      <ClientTestimonial />
+    </PublicLayout>
+  );
+}
